8574b034ff37f535f2e79b655bbcdc4dff198662
hagaitski <i.haga629@gmail.com>
2022-03-02 23:56:23 +0900

    Co-authored-by: Kentaro Matsushita <kentaro.m811@gmail.com> Co-authored-by: Sakamoto Keisuke <motikoma@users.noreply.github.com> Co-authored-by: Shogo Kato <kt-sygo0921@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# 20220302-mob
+# Yew todo list mob programming
+
+## 要件
+
+### 概要
+TODOアプリ
+※ 登録=インメモリ
+
+### やること
+
+- [x] 登録してあるタスク（タスク名）を表示できる
+  - [x] Structに状態としてタスクを保存
+  - [x] 状態からタスクを取得し、HTMLを作成
+- [ ] タスクを登録できる
+- [ ] タスクに完了フラグを追加し、ボタン一つでタスクを完了にできる
+- [ ] タスク名を編集できる
+
+## 使用技術
+
+- [yew](https://yew.rs/)
+
+## 環境構築
+
+
+### 必要ツール
+
+- cargo
+- trunk
+
+### コマンド
+
+```bash
+trunk serve
+```
+
+
+### 参考
+https://yew.rs/docs/getting-started/project-setup/using-trunk
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 use yew::prelude::*;
 
-struct Model;
+struct Model {
+    tasks: Vec<String>,
+}
 
 impl Component for Model {
     type Message = ();
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
-        Self
+        Self{
+            tasks: vec![
+                "task1".to_string(),
+                "task2".to_string(),
+                "task3".to_string(),
+            ],
+        }
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
@@ -15,7 +23,12 @@ impl Component for Model {
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
-        todo!()
+        let tasks = &self.tasks;
+        html!{
+            <ul>{tasks.into_iter().map(|task| {html! {
+                <li key={task.as_str()}>{task}</li>
+            }}).collect::<Html>()}</ul>
+        }
     }
 }
 
