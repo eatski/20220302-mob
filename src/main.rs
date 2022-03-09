05f31f409cc492528a27bbbaace5f99051cb8580
hagaitski <i.haga629@gmail.com>
2022-03-09 23:11:22 +0900

    fix: check todo Co-authored-by: Sakamoto Keisuke <motikoma@users.noreply.github.com> Co-authored-by: Kentaro Matsushita <kentaro.m811@gmail.com> Co-authored-by: yu-kgr <yu-kgr@users.noreply.github.com> Co-authored-by: y.hige <savacan@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,59 @@
 use yew::prelude::*;
 
+struct Task {
+    name: String,
+    done: bool,
+}
 struct Model {
-    tasks: Vec<String>,
+    tasks: Vec<Task>,
+}
+
+pub enum Msg {
+    SetInputChecked {
+        idx: usize
+    },
 }
 
 impl Component for Model {
-    type Message = ();
+    type Message = Msg;
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
         Self{
             tasks: vec![
-                "task1".to_string(),
-                "task2".to_string(),
-                "task3".to_string(),
+                Task {
+                    name: "task1".to_string(),
+                    done: false,
+                },
+                Task {
+                    name: "task2".to_string(),
+                    done: false,
+                },
+                Task {
+                    name: "task3".to_string(),
+                    done: true,
+                },
             ],
         }
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        todo!()
+        match msg {
+            Msg::SetInputChecked { idx } => {
+                self.tasks[idx].done = !self.tasks[idx].done;
+                true
+            }
+        }
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
+        let onclick = ctx.link().callback(|idx| Msg::SetInputChecked { idx });
         let tasks = &self.tasks;
         html!{
-            <ul>{tasks.into_iter().map(|task| {html! {
-                <li key={task.as_str()}>{task}</li>
+            <ul>{tasks.into_iter().enumerate().map(|(idx, task)| {html! {
+                <li key={task.name.as_str()} class={if task.done { "is-done" } else { "" }}>
+                    <input type="checkbox" checked={task.done} onclick={onclick.reform(move |_| idx).clone()} />{task.name.clone()}
+                </li>
             }}).collect::<Html>()}</ul>
         }
     }
